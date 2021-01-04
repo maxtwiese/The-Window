9e8c9a30513188ad145c15252f0463b3e8932ba1
Max <maxtwiese@gmail.com>
2021-01-04 15:52:30 -0800

    BASH prepocessing for label files.

--- src/labelwrangler.sh
+++ src/labelwrangler.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 # Builds a .csv for ETL of images and labels from UBIRIS Periodcular
-# Dataset. Run in the directory containing 'UBIPeriocular'.
+# Dataset
 
 # Move label files (.txt) to their own directory.
+cd /Users/maxwiese/Documents/DSI/assignments/The-Window/data #/path/to/data
 mkdir Periocular_Labels
-mv UBIPeriocular/*.txt Periocular_Labels
+mv UBIRISPrSmall/*.txt Periocular_Labels
+
+# Preprocess files for pandas.
+cd Periocular_Labels
+perl -i -pe 's/^.//' ./*
+perl -i -pe 's/Size;/Size1;Size2;Size3;/g' ./*
+perl -i -pe 's/;\n/,/g' ./*
+perl -i -pe 's/SizeY,C/SizeY\nC/g' ./*
+perl -i -pe 's/;/,/g' ./*
+sed -i '' -- '$ s/.$//' *
 
 # Extract labels from label files.
 function csv_builder {
@@ -17,7 +27,7 @@ dirpath = data + 'Periocular_Labels'
 output = data + 'labels.csv'
 
 pd.concat(
-pd.read_csv(os.path.join(dirpath, fname), sep=';')
+pd.read_csv(os.path.join(dirpath, fname))
 for fname in sorted(os.listdir(dirpath))
 ).to_csv(output)
 END
